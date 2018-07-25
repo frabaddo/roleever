24e641ce86b680ba553b1d687b7035503cfc4c0f
frabaddo <paradisecity93@gmail.com>
2018-07-25 20:40:58 +0200

    c

--- botapi/pauseableplus/pauseableplus.js
+++ botapi/pauseableplus/pauseableplus.js
@@ -1,4 +1,4 @@
-const pauseable = require('pauseable');
+const Pauseable = require('pauseable');
 const moment = require('moment');
 
 
@@ -6,7 +6,7 @@ var setTimeout=function(id,callback,time){
   timers[id].timestart=Date.now();
   timers[id].pausestart=0;
   timers[id].timeinpause=0;
-  timers[id].timer=pauseable.setTimeout(callback,time);
+  timers[id].timer=Pauseable.setTimeout(callback,time);
 }
 
 
