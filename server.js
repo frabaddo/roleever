09360763e3dd3249dab53de7f4da8b82c73a3d82
frabaddo <paradisecity93@gmail.com>
2018-08-03 17:12:22 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ function newmessage(msg,reply){
               replytousr.inlineKeyboard([
                 [{text:"Invia", callback_data: JSON.stringify({ action: "sendmessage",chatid: msg.chat.id, msgtxt: msg.text })},{text:"Annulla", callback_data: JSON.stringify({ action: "deletemessage",chatid: msg.chat.id, msgtxt: msg.text })}]
               ]);
-              replytousr.text("vuoi inviare questo messaggio? /n "+msg.text);
+              replytousr.text(msg.text);
               support.deletecmd(msg,reply);
               /*
               var timetoset=Date.now();
