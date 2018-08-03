95703c711dc6b2e80414a132375f7cad33b2724a
frabaddo <paradisecity93@gmail.com>
2018-08-03 21:56:26 +0200

    c

--- server.js
+++ server.js
@@ -276,7 +276,7 @@ function newmessage(msg,reply){
 
 
               var replytousr = bot.reply(msg.from.id);
-              var obj={ action: "sendmessage", chatid: eval(msg.chat.id) , msgtxt: msg.text.toString() };
+              var obj={ action: "sendmessage", chatid: msg.chat.id.toString() , msgtxt: msg.text.toString() };
               replytousr.inlineKeyboard([
                 [
                   {text:"Invia", callback_data: JSON.stringify(obj)},
