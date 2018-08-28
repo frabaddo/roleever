752082fbd10f66b91a122e638b5313efd324d01c
frabaddo <paradisecity93@gmail.com>
2018-08-28 12:24:11 +0200

    Add files via upload

--- botapi/createpg.js
+++ botapi/createpg.js
@@ -32,9 +32,9 @@ function modifystat(query,data,next){
     }
     if(user.phase==2){
       if(data.dir=="up"){
-        db.modifyobj("Users",{
-          eval("data.stat") : user[data.stat]+1,
-        },{ id: query.from.id , ready:false});
+        var x={};
+        x[data.stat]=user[data.stat]+1;
+        db.modifyobj("Users",x,{ id: query.from.id , ready:false});
       }
       else if(data.dir=="down"){
         var x={};
