883df87127f6aa1024372b9964e661fffd559c69
frabaddo <paradisecity93@gmail.com>
2018-10-29 12:06:11 +0100

    New text to toutube tutorial

--- text/textexport_ita.js
+++ text/textexport_ita.js
@@ -3,20 +3,8 @@ module.exports={
   pauseoff:"Sessione uscita dalla pausa!",
   sessionnotstarted: "Questa sessione non è ancora stata avviata, segui le istruzioni per iniziare a giocare",
   sessionnotcreated: "Questa sessione non è ancora stata creata, usa il comando /startbot e segui le istruzioni",
-  start:` benvenuto in RoleEver. Io sono il Masterbot, se sei un giocatore attendi la creazione della sessione per registrarti, se invece sei il futuro master segui questi semplici passi:
-  1. Crea un gruppo, trasformalo in supergruppo e solo successivamente inseriscici tutti i giocatori e me (RoleEverMaster).
-  2. Rendi amministratore solo me e te stesso. (Da ora nessuno potrà piu inviare messaggi nel gruppo finche il gioco non sarà avviato e sarà il suo turno, per comunicare ti consiglio di usare un secondo gruppo parallelo a questo)
-  3. Assicurati che ogni giocatore avvii la chat privata con me come tu stesso hai appena fatto, cercandomi su telegram e cliccando il tasto Avvia.
-  4. Assicurati che ogni giocatore, tu compreso, silenzi il gruppo di gioco.
-  5. Crea la sessione digitando "/startbot" (senza le virgolette) nel gruppo in cui mi hai inserito (solo tu devi farlo).
-  6. Assicurati che ogni giocatore si registri tramite il menu principale nel gruppo. ricorda che ci può essere un solo master.
-  7. Avviate la sessione, scegliendo una durata massima per i turni, dal menù principale( quando l'avrai avviata chiunque abbia terminato la creazione del proprio personaggio e il master potranno giocare).
-  Ogniqualvolta sarà il tuo turno scrivi un messaggio nel gruppo raccontando le gesta del tuo personaggio, dopo averlo inviato ti verrà chiesto di confermalo in privato.
-  Se il bot non dovesse rispondere ai tuoi comandi, rimuovilo dalla chat e reinsiriscilo rendendolo nuovamente amministratore. Per ulteriori bisogni contatta gli sviluppatori sull'apposito gruppo (https://t.me/joinchat/CkQUkFKv9CZ9Tob471YUEA).
-  Buon divertimento!!!`,
-  bootnogroup:"Ciao Sono un bot per giocare ai gdr su dispositivi mobile,\
-  per potermi utilizzare inseriscimi prima in un supergruppo e\
-  rendimi amministratore se lo hai già fatto, assicurati che sia un supergruppo",
+  start:`Benvenuto in RoleEver! Io sono il Masterbot! Per capire come utilizzarmi ti consigliamo di seguire i tutorial sul canale YouTube: “http://bit.ly/RoleEver”. Troverai tre video che spiegano come avviare una sessione o registrarti ad una esistente. Se hai domande o feedback potrai parlare direttamente con i miei sviluppatori sul seguente gruppo telegram: (https://t.me/joinchat/CkQUkFKv9CZ9Tob471YUEA).`,
+  bootnogroup:`Ciao Sono un bot per giocare ai gdr su dispositivi mobile, per potermi utilizzare segui le guide sul canale Youtube: “http://bit.ly/RoleEver” `,
 
   justcreate:"Sessione già creata e in corso",
 
@@ -32,12 +20,7 @@ module.exports={
 
   alreadycreating: "Per creare un nuovo personaggio occorre prima terminare tutti i personaggi in fase di creazione ",
 
-  masterturn:`Sessione avviata, Master è il tuo turno, inizia raccontando ai giocatori dove si trovano e cosa sta succedendo.
-  Ogni qualvolta invierete un messaggio vi verra chiesto di confermarlo nella chat privata.
-  Prima di confermare il messaggio potrete aggiungere un tiro di dado se siete giocatori oppure infliggere/curare ferite se siete il master.
-  E' stato creato di seguito il Menù di Gioco. Come si può notare sono presenti 4 pulsanti per gestire il gioco. Questo messaggio è stato fissato anche nella parte superiore del tuo schermo, cliccaci in qualsiasi momento per interagirvi comodamente.
-  Se cancellare per sbaglio il menù è sufficiente digitare "/menu" in chat (senza le virgolette).
-  Per ultimo si ricorda in caso di necessità il comando "/help" (in privato)`,
+  masterturn:`Sessione avviata! Al vostro turno, quando invierete un messaggio, dovrete confermarlo nella chat privata del bot. È stato creato di seguito il Menù di Gioco con 6 pulsanti per gestire la sessione. Questo messaggio è stato fissato anche nella parte superiore del tuo schermo, cliccaci in qualsiasi momento utilizzarlo. Master è il tuo turno! Inizia a raccontare ai giocatori dove si trovano e cosa sta succedendo. Dopo aver inviato i messaggi che desideri, seleziona “Passa Turno” dal menù.`,
 
   turnof:"È il turno di ",
 
@@ -57,12 +40,7 @@ module.exports={
 
   wanttosend:"Vuoi inviare questo messaggio?",
 
-  startregister:"Il master potrà avviare la sessione selezionando la durate massima che desidera per i turni di gioco.\n \
-  ricorda sempre i seguenti passaggi:\n \
-  1. Fai in modo che ogni giocatore mi avvii in privato.\n \
-  2. Fai in modo che ogni giocatore silenzi il gruppo di gioco\n \
-  3. Fai in modo che ogni giocatore si registri tramite il menù principale nel gruppo. ricorda che ci può essere un solo master.\n\
-  4. Avviate la sessione, scegliendo una durata massima per i turni, dal menù principale.",
+  startregister:`Sessione creata! Usa i comandi del menù per registrarti con il ruolo scelto. Se sei il master, dopo esserti registrato, potrai avviare la sessione selezionando la durata dei turni dei giocatori. Per la guida completa segui i tutorial sul canale YouTube linkato dal bot in privato.`,
 
   createpgcase0:"Complimenti per esserti registrato, per iniziare a creare il tuo personaggio scrivimi il nome che vuoi dargli",
 
